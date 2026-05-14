6cc5d99b31687987d91d912b914f374e6ef9bf99
Oifoghe-Emmanuel <157969112+Oifoghe-Emmanuel@users.noreply.github.com>
2026-05-14 09:47:49 +0100

    Bump CACHE_NAME to version 1.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'securephoto-v1.2.0'; // BUMP THIS ON EVERY RELEASE
+const CACHE_NAME = 'securephoto-v1.3.0'; // BUMP THIS ON EVERY RELEASE
 const urlsToCache = [
   './',
   './index.html',
